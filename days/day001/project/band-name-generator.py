025eb0e887a654e51552d9b590cd77a6f601179e
anya <anya>
2023-10-24 11:24:22 +0700

    add day001

---
+++ days/day001/project/band-name-generator.py
@@ -0,0 +1,14 @@
+#1 Greeting
+print("Hello")
+
+#2 ask the city the user grew up in
+city = input("What city do you grew up in?\n")
+
+#3 ask user pet name
+pet = input("What is the name of your pet?\n")
+
+#4 combine city and pet
+band = city + " " + pet
+
+#5 print band
+print("Your band name is " + band)
